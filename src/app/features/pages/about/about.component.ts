00981a1930ff61b2a825623262d1a0563941ec08
Ian Gerolamo <iangerolamo@gmail.com>
2022-07-15 14:15:09 -0300

    Novos ajustes

--- src/app/features/pages/about/about.component.ts
+++ src/app/features/pages/about/about.component.ts
@@ -3,30 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goToLink(url: string) {
+    window.open(url, '_blank');
   }
-  
+
   resume() {
-    window.location.href = "https://orcid.org/0000-0003-1819-5371";
+    window.location.href = 'https://orcid.org/0000-0003-1819-5371';
   }
 
   lattes() {
-    window.location.href = "http://buscatextual.cnpq.br/buscatextual/visualizacv.do?metodo=apresentar&id=K4323228Z4";
+    window.location.href =
+      'http://buscatextual.cnpq.br/buscatextual/visualizacv.do?metodo=apresentar&id=K4323228Z4';
   }
 
   googleScholar() {
-    window.location.href = "https://scholar.google.com.br/citations?user=10xWaMQAAAAJ&hl=pt-BR";
+    window.location.href =
+      'https://scholar.google.com.br/citations?user=10xWaMQAAAAJ&hl=pt-BR';
   }
 
   researchGate() {
-    window.location.href = "https://www.researchgate.net/profile/Caian-Gerolamo";
+    window.location.href =
+      'https://www.researchgate.net/profile/Caian-Gerolamo';
   }
-
-
 }
